efb3fb5b22a6744f951b69aadaf49a274f5d72d7
Fabio Rodriguez <frodriguez@canterburyfortmyers.org>
2020-09-29 18:53:22 -0400

    added thirdperson and firstperson switching

---
+++ Assets/SwitchCamera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class SwitchCamera : MonoBehaviour
+{
+    
+    public Camera tps;
+    public Camera fps;
+    bool isFps = true;
+    DepthOfField depthOfField;
+    public PostProcessVolume fpsvolume;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fpsvolume.profile.TryGetSettings(out depthOfField);
+        fps.enabled = isFps;
+        tps.enabled = !isFps;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            isFps = !isFps;
+            fps.enabled = isFps;
+            tps.enabled = !isFps;
+            fpsvolume.isGlobal = !tps.enabled;
+            
+        }
+
+    }
+}
